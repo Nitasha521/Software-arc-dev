7e668571e26b7617003dad294361575f38585c40
Nitasha521 <24790786@stu.mmu.ac.uk>
2026-02-05 14:08:25 +0000

    commit

---
+++ week2/src/DiceShaker.java
@@ -0,0 +1,14 @@
+
+public interface DiceShaker {
+    int next();
+}
+public class RandomSingleDiceShaker implements DiceShaker {
+
+    private static final Random random = new Random();
+
+    @Override
+    public int next() {
+        //nextInt Returns a pseudorandom value between 0 (inclusive) and the specified value (exclusive), so we need to add one to get a range of 1 to 6.
+        return random.nextInt(6) + 1;
+    }
+}
